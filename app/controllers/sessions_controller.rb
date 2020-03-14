414416bc275de51aebc2ec4f1926119761c55a25
fukudome <fuku.noir9582@gmail.com>
2020-03-14 16:15:19 +0000

    ログインフォーム修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   
   
   def create
+    binding.pry
     user = User.find_by(name: params[:session][:name])
       if user && user.authenticate(params[:session][:password])
         log_in user
